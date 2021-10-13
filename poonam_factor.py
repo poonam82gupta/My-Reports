9913d458dc391a4487568449ebdb5433c0ac1ab9
poonam <poonam82gupta@gmail.com>
2021-10-13 10:39:53 -0400

    display factor of 24

---
+++ poonam_factor.py
@@ -0,0 +1,26 @@
+
+number=24
+print(f"the factors of a {number} are :" )
+for i in range(1,number+1):
+    if(number%i==0):
+        print(i)
+
+
+# below prog count the number of vowels in a string. This program was made by a genius called
+# Aarna the Greatest
+
+"""
+        
+name=input("name")
+count =0
+for i in name:
+#    print(i)
+    if i in ('o',"a","e","i","u", 'A', 'E','I',\
+             "O",'U'):
+        count = count+1
+print ("the numnber of vowel in name is", count) 
+
+fn=int(input("please enter your your first number"))
+sn=int(input("please enter your second number
+             
+"""
